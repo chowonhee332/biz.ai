452d40f5283eeaaba5d8665da62cab95a7f0f6d6
조원희 <wonhee.cho@jowonhuiui-MacBookPro.local>
2026-03-26 13:59:27 +0900

    feat: 히어로 Spline 대지 1000x616, 반응형 scale 및 포지션 조정

--- src/components/HeroSpline.tsx
+++ src/components/HeroSpline.tsx
@@ -4,12 +4,12 @@ const Spline = lazy(() => import('@splinetool/react-spline'));
 
 export default function HeroSpline() {
   return (
-    <div className="absolute top-[130%] md:top-[95%] left-[150%] md:left-[170%] lg:left-[120%] -translate-x-1/2 -translate-y-1/2">
-      <div className="scale-[1] md:scale-[1.2] origin-center">
+    <div className="absolute top-1/2 left-[80%] -translate-x-1/2 -translate-y-1/2">
+      <div className="scale-[1] md:scale-[1.2] lg:scale-[1.25] origin-center">
         <Suspense fallback={null}>
           <Spline
             scene="https://prod.spline.design/mI8hnuZfc0mxniMC/scene.splinecode"
-            style={{ width: 2000, height: 1231 }}
+            style={{ width: 1000, height: 616 }}
           />
         </Suspense>
       </div>
